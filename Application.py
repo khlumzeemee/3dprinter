678c060b19e30d1fdb78fa84f283b2bd228e133c
Khlumzeemee <asooredoo@gmail.com>
2015-11-26 15:42:59 +0200

    Update Application.py

--- Application.py
+++ Application.py
@@ -2,6 +2,7 @@ from Tkinter import Tk, RAISED, BOTH
 from Ttk import Frame, Button, Style, Label
 
 class Application(Frame):
+  """ Main class """
   
   def __init__(self,master):
     Frame.__init__(self,master)
@@ -12,15 +13,47 @@ class Application(Frame):
     self.master.title("3D Printer")
     self.style = Style()
     
-    frame = Frame(self, relief = RAISED, borderwidth = 1)
-    frame.pack(fill = BOTH, expand = 1)
+    #Create two screens
+    self.create_customer = CreateCustomer(self)
+    self.execute_script = ExecuteScript(self)
     
-    self.pack(fill = BOTH, expand = 1)
+
+class CreateCustomer(Frame):
+  """Create customer screen"""
+  
+  def __init__(self, master):
+    Frame.__init__(self,master)
+    self.master = master
+    self.initUI()
+  
+  def self.initUI(self):
+    #TODO add components here
+    #Labels and inputtexts
     
-    closeButton = Button(self, text="Close")
-    closeButton.pack(side=RIGHT, padx=5, pady=5)
-    okButton = Button(self, text="OK")
-    okButton.pack(side=RIGHT)
+    #Create Customer Button
+    #Saving to DB and generating an ID
+    #Redirecting to the next screen
+    pass
+
+class ExecuteScript(Frame):
+  """Execute script screen"""
+  
+  def __init__(self, master):
+    Frame.__init__(self,master)
+    self.master = master
+    self.initUI()
+  
+  def self.initUI(self):
+    #TODO add components here
+    
+    #Customer summary / id fields
+    
+    #Take picture button
+    
+    #Update button
+    
+    #Input text to display the script result
+    pass
 
 def main():
   
