ece6286e732decdc6dcd7c65cc09ae4b3cf16664
Allen Sooredoo <oreo@Allens-MacBook-Pro.local>
2015-11-27 00:17:16 +0200

    Added customer class

--- Application.py
+++ Application.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, RAISED, BOTH, LEFT, TOP, RIGHT, BOTTOM
+from tkinter import Tk, RAISED, BOTH, LEFT, TOP, RIGHT, BOTTOM, messagebox
 from tkinter.ttk import Frame, Button, Style, Label, Entry
 
 class Application(Frame):
@@ -33,20 +33,21 @@ class CreateCustomer(Frame):
     self.style = Style()
 
     default_padding = {'padx': 10, 'pady' : 10}
-    
+
+    #TODO change alignment    
     #Left frame for labels
     self.left_frame = Frame(self.master)
     
-    self.name_label = Label(self.left_frame, text = "Name")
+    self.name_label = Label(self.left_frame, text = "Name", style="BW.TLabel")
     self.name_label.pack(default_padding)
 
-    self.surname_label = Label(self.left_frame, text = "Surname")
+    self.surname_label = Label(self.left_frame, text = "Surname", style="BW.TLabel")
     self.surname_label.pack(default_padding)    
 
-    self.email_label = Label(self.left_frame, text = "Email")
+    self.email_label = Label(self.left_frame, text = "Email", style="BW.TLabel")
     self.email_label.pack(default_padding)
 
-    self.cellphone_label = Label(self.left_frame, text = "Cellphone")
+    self.cellphone_label = Label(self.left_frame, text = "Cellphone", style="BW.TLabel")
     self.cellphone_label.pack(default_padding)
 
     #Right frame for entries
@@ -66,25 +67,44 @@ class CreateCustomer(Frame):
     
     #Bottom frame for button
     self.button_frame = Frame(self.master)
-    self.create_customer_button = Button(self.button_frame, text = "Create Customer")
+    self.create_customer_button = Button(self.button_frame, text = "Create Customer", command = self.save_customer)
     self.create_customer_button.pack(default_padding)
 
     self.button_frame.pack(side = BOTTOM, fill = "x", expand = True)
     self.left_frame.pack(side = LEFT, expand = True, fill = "y")
     self.right_frame.pack(side = RIGHT, expand = True, fill = "y")
 
-
-    #Create Customer Button
-    #Saving to DB and generating an ID
-    #Redirecting to the next screen
     self.frame.pack()
+
+  def save_customer(self):
+    print("saving customer")
+
+    self.customer = Customer(self.name_entry.get(), self.surname_entry.get(),
+                             self.email_entry.get(), self.cellphone_entry.get())
+    if not self.customer.validate():
+      messagebox.showerror(message = "All fields are mandatory, please fill in all the fields")
+      return
+    
+    #TODO tinydb save
+    #TODO redirect to next page
   
   def go_to_execute_script(self):
     """redirect to next page"""
     self.execute_script = Toplevel(self, self.master)
     self.app = ExecuteScript(self.execute_script)
      
+class Customer(object):
 
+  def __init__(self, name, surname, email, cellphone):
+    self.name = name
+    self.surname = surname
+    self.email = email
+    self.cellphone = cellphone
+
+  def validate(self):
+    return self.name and self.surname and self.email and self.cellphone
+    
+    
 class ExecuteScript(Frame):
   """Execute script screen"""
   
@@ -108,7 +128,6 @@ class ExecuteScript(Frame):
 def main():
   
     root = Tk()
-    root.geometry("300x200+300+300")
     app = CreateCustomer(root)
     root.mainloop()  
 
