c306aa50a60fca9ce2c810de114a229e035bd361
Khlumzeemee <asooredoo@gmail.com>
2015-11-27 17:35:11 +0200

    Update ApplicationMVC.py

--- ApplicationMVC.py
+++ ApplicationMVC.py
@@ -2,11 +2,50 @@ from tkinter import Tk, RAISED, BOTH, LEFT, TOP, RIGHT, BOTTOM, messagebox
 from tkinter.ttk import Frame, Button, Style, Label, Entry
 from tinydb import Query
 
+class ApplicationController(Tk):
+  def __init__(self, *args, **kwargs):
+      Tk.__init__(self, *args, **kwargs)
+  
+      # the container is where we'll stack a bunch of frames
+      # on top of each other, then the one we want visible
+      # will be raised above the others
+      container = Frame(self)
+      container.pack(side="top", fill="both", expand=True)
+      container.grid_rowconfigure(0, weight=1)
+      container.grid_columnconfigure(0, weight=1)
+  
+      self.frames = {}
+      for F in (CreateCustomerView, ExecuteScriptView):
+          frame = F(container, self)
+          self.frames[F] = frame
+          # put all of the pages in the same location;
+          # the one on the top of the stacking order
+          # will be the one that is visible.
+          frame.grid(row=0, column=0, sticky="nsew")
+  
+      self.show_frame(StartPage)
+  
+  def show_frame(self, c):
+      '''Show a frame for the given class'''
+      frame = self.frames[c]
+      frame.tkraise()
+      
+  def save_customer(self, * args, **kwargs):
+    customer_id = self.model.save_customer(*args, **kwargs)
+    
+    show_frame(ExecuteScriptView)
+
+  def run_take_picture_script(self):
+    pass
+  
+  def run_update_script(self):
+    pass
+            
 class CreateCustomerView(Frame):
-  def __init__(self, master):
+  def __init__(self, master, controller):
     self.master = master
     self.master.title("3d Printer - Create customer")
-    self.controller = None
+    self.controller = controller
     self.entries = []
   
   def register(self, controller):
@@ -29,7 +68,13 @@ class CreateCustomerView(Frame):
     button_frame = Frame(self.master)
     Button(self.button_frame, text = "Create Customer", command = self.controller.save_customer).pack(default_padding)
     button_frame.pack(expand = True, fill = "x")
-  
+
+class ExecuteScriptView(Frame):
+  def __init__(self, master, controller):
+    self.master = master
+    self.master.title("3d Printer - Execute script")
+    self.controller = controller
+
 class CreateCustomerModel(object):
   def __init__(self):
     self.db = TinyDB('/db/db.json')
@@ -65,6 +110,9 @@ class ApplicationController(object):
         frame = self.frames[c]
         frame.tkraise()  
   
+app = ApplicationView()
+app.mainloop()
+
 root = Tk()
 toplevel_view = Toplevel(root)
 
